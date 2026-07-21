19e55737c49e1f3351510be421afc220259083c1
TendoJakarta249 <dod35945@gmail.com>
2026-07-21 16:04:25 +0700

    Time: 0 ms (100%), Space: 10.6 MB (31.83%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.cpp
+++ 0812-largest-triangle-area/0812-largest-triangle-area.cpp
@@ -29,3 +29,22 @@ public:
         return ans;
     }
 };
+
+    /* int maxarea = 0;
+    for (int i=0; i<points.length-2 ; i++){
+        for (int j=i+1; j<points.length-1 ; j++){
+            for (int k=j+1; k<points.length ; k++){
+                int x1,y1,x2,y2;
+                x1 = points[i][0] - points[j][0];
+                y1 = points[i][1] - points[j][1];
+                x2 = points[i][0] - points[k][0];
+                y2 = points[i][1] - points[k][1];
+                int area = x1*y2 - x2*y1; // rectangle area, triangle is half
+                if (area < 0){
+                    area = -area;
+                if (area > maxarea){
+                    maxarea = area;
+                }
+            }
+        }
+        return maxarea/2.0; // triangle area is half */
